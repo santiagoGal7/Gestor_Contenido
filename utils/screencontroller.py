1b4fa2cbc80c75e0a77f61b5fbfe219826d6b060
santiagoGal7 <212437375+santiagoGal7@users.noreply.github.com>
2025-07-31 15:29:55 -0500

    feat: :sparkles: new functions to show
    three new functions were created to show the user data from the program, and receive the user's typed data.

--- utils/screencontroller.py
+++ utils/screencontroller.py
@@ -59,3 +59,57 @@ def main_menu_set(opciones, titulo):
         elif tecla == "ENTER":
             limpiar_pantalla()
             return seleccion_actual
+
+
+def obtener_input_valido(prompt, es_opcional=False, validacion=None, msg_error="Entrada inválida."):
+
+    print(f"\n{styles.COLOR_YELLOW}(Escribe !cancelar para anular la operación){styles.COLOR_RESET}")
+    while True:
+        print(f"{styles.COLOR_CYAN}{prompt}{styles.COLOR_RESET}", end=" ")
+        entrada = input().strip()
+
+        if entrada == '!cancelar':
+            return None
+
+        if not entrada and not es_opcional:
+            mostrar_mensaje("Error: Este campo no puede estar vacío.", "error", con_pausa=False)
+            continue
+        if es_opcional and not entrada:
+            return ""
+        if validacion and not validacion(entrada):
+            mostrar_mensaje(f"Error: {msg_error}", "error", con_pausa=False)
+            continue
+        return entrada
+
+def mostrar_mensaje(mensaje, tipo="info", con_pausa=True):
+    color_map = {
+        "exito": styles.COLOR_GREEN, "aviso": styles.COLOR_YELLOW,
+        "error": styles.COLOR_RED, "info": styles.COLOR_BLUE
+    }
+    print(f"\n{color_map.get(tipo, styles.COLOR_BLUE)}{styles.COLOR_BOLD}{mensaje}{styles.COLOR_RESET}")
+    if con_pausa:
+        pausar_pantalla()
+
+def mostrar_tabla(coleccion, titulo):
+    limpiar_pantalla()
+    if not coleccion:
+        mostrar_mensaje(f"No hay elementos para mostrar en esta vista.", "aviso")
+        return
+
+    print(f"{styles.COLOR_BOLD}{styles.COLOR_BLUE}{titulo}{styles.COLOR_RESET}\n")
+    cabeceras = ["ID", "Categoría", "Título", "Responsable", "Género", "Valoración"]
+    
+    print(f"{styles.COLOR_GREEN}{cabeceras[0]:<5} {cabeceras[1]:<10} {cabeceras[2]:<30} {cabeceras[3]:<25} {cabeceras[4]:<20} {cabeceras[5]:<10}{styles.COLOR_RESET}")
+    print("-" * 105)
+
+    for item in coleccion:
+        valoracion = item.get('valoracion') or 'N/A'
+        print(
+            f"{item['id']:<5} "
+            f"{item['categoria']:<10} "
+            f"{item['titulo']:<30.30} "
+            f"{item['responsable']:<25.25} "
+            f"{item['genero']:<20.20} "
+            f"{str(valoracion):<10}"
+        )
+    pausar_pantalla()
